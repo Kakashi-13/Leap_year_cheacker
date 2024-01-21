e8d61462fd2086d6a299ff07e1fcad3fc655a544
Jignesh Purohit <156500596+Kakashi-13@users.noreply.github.com>
2024-01-21 14:37:57 +0530

    Add files via upload
    run fifth.py file

---
+++ fifth.py
@@ -0,0 +1,9 @@
+fh = open('output5.txt','w')
+year = int(input("Enter the Year : "))
+fh.write("year is "+str(year)+'\n')
+
+if (year % 4 == 0 and year % 100 != 0 or year % 400 != 0):
+    fh.write("It's a leap year "+'\n')
+else:
+    fh.write("It's NOt a Leap year"+'\n')
+fh.write("--------------------------------------------------")
